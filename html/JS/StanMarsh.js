aec24b9b80e61a1d87a5723c61c02ca0fe602bd4
3Morning <madrugadaanderson@gmail.com>
2024-06-26 22:54:58 -0300

    SouthPark

---
+++ html/JS/StanMarsh.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+        dots: true,
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 800,
+    });
+
+    $('.prev-btn').click(function(){
+        $('.carousel').slick('slickPrev');
+    });
+
+    $('.next-btn').click(function(){
+        $('.carousel').slick('slickNext');
+    });
+})
